cff9cf2323ebaa8278372ceb2ff842fd72c8f5ab
Sirion <thesirion@gmail.com>
2020-05-10 19:20:56 -0300

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-angular-91620',
-    user : 'matheusribeiro',
-    password : 'sqlHeavyWeapon',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
