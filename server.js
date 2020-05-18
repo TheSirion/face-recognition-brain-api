8e735b1ecf47dc96d17219ff67871c9c0d6a01ed
Sirion <thesirion@gmail.com>
2020-05-18 14:59:22 -0300

    fixing CORS origin 2

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ db.select('*').from('users').then(data => {
 const app = express();
 app.use(express.json());
 app.use(cors({
-  origin: 'https://thesirion.github.io/'
+  origin: 'https://thesirion.github.io'
 }));
 
 app.get('/', (req, res) => {
